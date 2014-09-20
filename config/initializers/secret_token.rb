93d4184faf75df999aa028e8d5db021826fb984d
codesterkemp <sparky_csk@yahoo.com>
2014-09-20 05:25:27 -0400

    base rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FarrierSite::Application.config.secret_token = '5a25e12f779d2beb75de39c0da44287b3da0cb1c2343a8ffb89e4e81aa01ad0abe48117b05300fb7159de55dcb8e1edff7ea6c876f21ef32cc39bf1187a90a02'
